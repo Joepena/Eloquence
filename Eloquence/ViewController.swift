6f5f0c532e75c8399ca0d14fd5df494f85cccf85
Joe <josephp1995@ufl.edu>
2017-01-21 02:16:46 -0500

    initial commit

---
+++ Eloquence/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Eloquence
+//
+//  Created by Joseph Pena on 1/21/17.
+//  Copyright © 2017 Joseph Pena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
